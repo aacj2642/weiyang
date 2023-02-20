ce37602c0576e2361d2527ceeb1fc3da9fafcccb
aacj2642 <aacj2642@gmail.com>
2023-02-21 01:16:08 +0800

    增加套件，整理專案

--- src/main.js
+++ src/main.js
@@ -1,14 +1,42 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import Loading from "vue-loading-overlay";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/all.scss";
+import "./assets/main.css";
 
-const app = createApp(App)
+import { defineRule, Form, Field, ErrorMessage, configure } from "vee-validate";
+import { required, email, min, max } from "@vee-validate/rules";
 
-app.use(createPinia())
-app.use(router)
+defineRule("required", required);
+defineRule("email", email);
+defineRule("min", min);
+defineRule("max", max);
 
-app.mount('#app')
+import { localize, setLocale } from "@vee-validate/i18n";
+import zh_TW from "@vee-validate/i18n/dist/locale/zh_TW.json";
+
+configure({
+  generateMessage: localize({ zh_TW }), // 載入繁體中文語系
+  validateOnInput: true, // 當輸入任何內容直接進行驗證
+});
+setLocale("zh_TW");
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(VueAxios, axios);
+
+app.component("VueLoading", Loading);
+
+app.component("VForm", Form);
+app.component("VField", Field);
+app.component("ErrorMessage", ErrorMessage);
+
+app.mount("#app");
