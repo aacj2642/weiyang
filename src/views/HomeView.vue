1cec46c71113ca6450b868796ee4bdcb41132314
aacj2642 <aacj2642@gmail.com>
2023-03-05 23:38:37 +0800

    add banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,41 @@
 <template>
-  <div>
-    <div class="bg-success mb-2">This is an home page</div>
+  <div
+    class="banner"
+    style="
+      background-image: linear-gradient(
+          rgba(var(--bs-primary-rgb), 1) 0%,
+          rgba(var(--bs-primary-rgb), 0.7) 20%,
+          rgba(var(--bs-primary-rgb), 0.3) 30%,
+          rgba(var(--bs-primary-rgb), 0) 100%
+        ),
+        url('/group_photo.jpg');
+    "
+  >
+    <img src="/hero-text.png" alt="weiyang" class="banner-text" />
   </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../scss/customVariables";
+.banner {
+  max-width: 100%;
+  width: 100vw;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  background-position: center bottom -20px;
+  height: 480px;
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    background-position: center bottom -40px;
+    height: 800px;
+  }
+}
+.banner-text {
+  display: block;
+  width: 320px;
+  margin: 35px auto 0;
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    width: 460px;
+  }
+}
+</style>
