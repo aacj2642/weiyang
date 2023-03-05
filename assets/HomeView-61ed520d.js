0ef823393148d3f056462c063c2602885ee7cfda
aacj2642 <aacj2642@gmail.com>
2023-03-05 23:44:09 +0800

    deploy

---
+++ assets/HomeView-61ed520d.js
@@ -0,0 +1,7 @@
+import{_ as a,o as r,c as s,p as o,a as t,b as n}from"./index-654546f6.js";const _="/weiyang/hero-text.png";const c={},p=e=>(o("data-v-52432db7"),e=e(),t(),e),i={class:"banner",style:{"background-image":`linear-gradient(
+          rgba(var(--bs-primary-rgb), 1) 0%,
+          rgba(var(--bs-primary-rgb), 0.7) 20%,
+          rgba(var(--bs-primary-rgb), 0.3) 30%,
+          rgba(var(--bs-primary-rgb), 0) 100%
+        ),
+        url('./group_photo.jpg')`}},d=p(()=>n("img",{src:_,alt:"weiyang",class:"banner-text"},null,-1)),b=[d];function g(e,l){return r(),s("div",i,b)}const u=a(c,[["render",g],["__scopeId","data-v-52432db7"]]);export{u as default};
