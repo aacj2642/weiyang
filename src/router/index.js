38076f55014aa0c5bf40c9df53a5982d66091bb0
aacj2642 <aacj2642@gmail.com>
2026-03-15 19:56:52 +0800

    修正成員詳細頁面

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
+      path: "/member/:id",
+      name: "member-detail",
+      component: () => import("../views/MemberDetailView.vue"),
+    },
+    {
       path: "/member",
       name: "member",
       component: () => import("../views/MemberView.vue"),
